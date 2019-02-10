4fd8e316a5ab76087c8cf4d8ed6ed6d7b62145dc
Cher Stewart <cstewart@cstewart017338.starbucks.net>
2019-02-09 19:51:05 -0600

    Connects to redis client.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import redis from 'redis'
+
+require('dotenv').config()
+
+const redisClient = redis.createClient(process.env.REDIS_URL)
 
 const app = express()
-
 app.all('/spotify/data/:key', (req, res) => {
   res.send('Success! 🎉\n')
 })
