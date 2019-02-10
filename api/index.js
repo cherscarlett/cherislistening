768bd2745535d90ebe3b2300df3a321f009210d2
Cher Stewart <cstewart@cstewart017338.starbucks.net>
2019-02-09 19:52:53 -0600

    Adds event listeners to redis client.

--- api/index.js
+++ api/index.js
@@ -4,8 +4,17 @@ import redis from 'redis'
 require('dotenv').config()
 
 const redisClient = redis.createClient(process.env.REDIS_URL)
+// Redis client
+redisClient.on('connect', () => {
+  console.log('\n🎉 Redis client connected 🎉\n')
+})
+
+redisClient.on('error', err => {
+  console.error(`\n🚨 Redis client could not connect: ${err} 🚨\n`)
+})
 
 const app = express()
+// Express app
 app.all('/spotify/data/:key', (req, res) => {
   res.send('Success! 🎉\n')
 })
